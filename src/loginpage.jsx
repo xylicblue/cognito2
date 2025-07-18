2b2b73bc09086027eb3bfb69c8b010aa38ac8014
mahad bin aamir <26100249@lums.edu.pk>
2025-07-18 15:11:12 +0500

    done

--- src/loginpage.jsx
+++ src/loginpage.jsx
@@ -6,10 +6,10 @@ const LoginPage = () => {
   const VITE_COGNITO_DOMAIN = import.meta.env.VITE_COGNITO_DOMAIN;
   const VITE_COGNITO_CLIENT_ID = import.meta.env.VITE_COGNITO_CLIENT_ID;
   const VITE_COGNITO_REDIRECT_URI = import.meta.env.VITE_COGNITO_REDIRECT_URI;
-  const login_hint = "abdul.wahid@quickstart.com";
+  // const login_hint = "abdul.wahid@quickstart.com";
   // -----------------------------
 
-  const loginUrl = `https://${VITE_COGNITO_DOMAIN}/authorize?response_type=code&client_id=${VITE_COGNITO_CLIENT_ID}&redirect_uri=${VITE_COGNITO_REDIRECT_URI}&scope=email+openid+profile&login_hint=${login_hint}`;
+  const loginUrl = `https://${VITE_COGNITO_DOMAIN}/authorize?response_type=code&client_id=${VITE_COGNITO_CLIENT_ID}&redirect_uri=${VITE_COGNITO_REDIRECT_URI}&scope=email+openid+profile`;
 
   return (
     <div className="auth-container">
